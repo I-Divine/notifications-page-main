59bd7e3feb2f510945d9e639d600bb3daace1bf5
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-05-01 09:27:50 +0100

    Created new components

---
+++ src/components/notification-details.jsx
@@ -0,0 +1,11 @@
+const NotifDetails = () => {
+  return (
+    <div className="notif-details">
+      <span className="notif-sender">Angela Gray</span>
+      <span className="notif-action">reacted to your post</span>
+      <span className="notif-item">My first tournament today!</span>
+      <p className="notif-time">1m ago</p>
+    </div>
+  );
+};
+export default NotifDetails;
--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -1,21 +1,13 @@
+import NotifDetails from "./notification-details";
+import NotifMessage from "./notification-msg";
+
 const Notification = () => {
   return (
     <div className="notification">
       <img src="/images/avatar-angela-gray.webp" alt="" />
       <div>
-        <div className="notif-details">
-          <span className="notif-sender">Angela Gray</span>
-          <span className="notif-action">reacted to your post</span>
-          <span className="notif-item">My first tournament today!</span>
-          <p className="notif-time">1m ago</p>
-        </div>
-        <div className="notif-msg">
-          <p>
-            Hello thank you for setting up the chess club. I've been a member
-            for a few weeks now and I'm already having lots of fun and improving
-            my game.
-          </p>
-        </div>
+        <NotifDetails />
+        <NotifMessage></NotifMessage>
       </div>
     </div>
   );
